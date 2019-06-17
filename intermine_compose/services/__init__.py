8b4418693a24f427d36b59f043ffba49a5dab8be
Ankur Kumar <ank@leoank.me>
2019-06-17 08:47:46 +0530

    Add init method for services module

---
+++ intermine_compose/services/__init__.py
@@ -0,0 +1,8 @@
+from .mail import mail
+from .celery import make_celery
+from .kubernetes import init_kubernetes
+
+def init_app(app):
+    mail.init_app(app)
+    make_celery(app)
+    init_kubernetes(app)
