6608937aaa14a2fa36c283f92752d50b07ef445f
Ankur Kumar <ank@leoank.me>
2019-06-17 08:48:54 +0530

    Add config module

---
+++ config/default.py
@@ -0,0 +1,8 @@
+import os
+
+HOME = os.environ.get("HOME")
+CELERY_RESULT_BACKEND = "redis"
+CELERY_BROKER_URL = "redis://127.0.0.1:6379"
+KUBERNETES_CONFIG_PATH = f"{HOME}/.kube/config"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
