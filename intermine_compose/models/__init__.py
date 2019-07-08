8ed0acd9f09ca73e26b96b34fe887683cefb6d1e
Ankur Kumar <ank@leoank.me>
2019-07-08 07:29:21 +0530

    Add functions to create and drop all tables

--- intermine_compose/models/__init__.py
+++ intermine_compose/models/__init__.py
@@ -1,4 +1,11 @@
 from .base import db
+from .users import User
 
 def init_app(app):
     db.init_app(app)
+
+def create_all_tables():
+    db.create_all()
+
+def drop_all_tables():
+    db.drop_all()
