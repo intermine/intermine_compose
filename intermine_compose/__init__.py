66eaf43107c28dde97016593829358e4ce1c1b0d
Ankur Kumar <ank@leoank.me>
2019-07-08 07:30:42 +0530

    Add drop tables during init when in dev mode
    This will help to bypass running migrations during dev stage

--- intermine_compose/__init__.py
+++ intermine_compose/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import os
 
 def create_app():
     from . import models, routes, services
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config.default')
     app.config.from_envvar('FLASK_CONFIG_FILE', silent=True)
-    models.init_app(app)
     routes.init_app(app)
     services.init_app(app)
+    models.init_app(app)
+    app.app_context().push()
+    if os.environ.get("DEV_DB"):
+        models.drop_all_tables()
+    models.create_all_tables()
     return app
