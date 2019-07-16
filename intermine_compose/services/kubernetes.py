afb19c103a8f64ee0ac7aac46df56f73bf447ddf
Ankur Kumar <ank@leoank.me>
2019-07-17 02:29:56 +0530

    load kuberenetes config based on env
    - if deployed inside a kubernetes cluster then incluster config will be used
    - if deployed outside the cluster then  config will be loaded from a file

--- intermine_compose/services/kubernetes.py
+++ intermine_compose/services/kubernetes.py
@@ -1,6 +1,10 @@
 from kubernetes import client, config
+import os
 
 def init_kubernetes(app):
-    config.load_kube_config(app.config["KUBERNETES_CONFIG_PATH"])
+    if os.environ.get("FLASK_CONFIG_MODE", default=False) is not "production": 
+        config.load_kube_config(app.config["KUBERNETES_CONFIG_PATH"])
+    else:
+        config.load_incluster_config()
 
 
