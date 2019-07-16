6b178e769b39c5baa55a1d89508120d12c870d62
Ankur Kumar <ank@leoank.me>
2019-07-17 03:44:52 +0530

    Add quota colums to user model

--- intermine_compose/models/users.py
+++ intermine_compose/models/users.py
@@ -1,6 +1,7 @@
 from .base import db, TimestampMixin
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String,Integer
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 class User(TimestampMixin, db.Model):
     id = Column(UUID(as_uuid=True), primary_key=True)
@@ -9,3 +10,8 @@ class User(TimestampMixin, db.Model):
     organization = Column(String(80))
     email = Column(String(120), unique=True, nullable=False)
     pwd_hash = Column(String(80), nullable=False)
+    mines = relationship("Mine", back_populates="user", lazy="joined")
+    active_mine_quota = Column(Integer, default=1)
+    inactive_mine_quota = Column(Integer, default=5)
+    daily_mine_build_quota = Column(Integer, default=5)
+    storage_quota = Column(Integer, default=500)
