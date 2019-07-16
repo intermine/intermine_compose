b67c91d5827413ec393889643b678aac70d8802e
Ankur Kumar <ank@leoank.me>
2019-07-17 02:28:23 +0530

    Add timestampMixin to user model

--- intermine_compose/models/users.py
+++ intermine_compose/models/users.py
@@ -1,10 +1,11 @@
-from .base import db
+from .base import db, TimestampMixin
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 
-class User(db.Model):
+class User(TimestampMixin, db.Model):
     id = Column(UUID(as_uuid=True), primary_key=True)
     firstname = Column(String(80))
     lastname = Column(String(80))
     organization = Column(String(80))
     email = Column(String(120), unique=True, nullable=False)
+    pwd_hash = Column(String(80), nullable=False)
