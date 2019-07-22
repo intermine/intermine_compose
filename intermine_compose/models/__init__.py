0eeea583aa55d1c81bc9a8b9395e352703c8a035
Ankur Kumar <ank@leoank.me>
2019-07-22 05:56:36 +0530

    add marshmallow init to db models init

--- intermine_compose/models/__init__.py
+++ intermine_compose/models/__init__.py
@@ -1,8 +1,12 @@
-from .base import db
+from .base import db, ma
 from .users import User
+from .mine import Mine
+from .templates import Template
 
 def init_app(app):
     db.init_app(app)
+    ma.init_app(app)
+
 
 def create_all_tables():
     db.create_all()
--- intermine_compose/models/base.py
+++ intermine_compose/models/base.py
@@ -1,8 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy import DateTime, Column
+from flask_marshmallow import Marshmallow
+
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 class TimestampMixin(object):
     created = Column(
