3bdb0616b53fca7d51936d4d0642d2d526e62762
Ankur Kumar <ank@leoank.me>
2019-07-22 08:52:06 +0530

    Made Kube feauture optional with an env var

--- intermine_compose/services/kubernetes.py
+++ intermine_compose/services/kubernetes.py
@@ -2,9 +2,11 @@ from kubernetes import client, config
 import os
 
 def init_kubernetes(app):
-    if os.environ.get("FLASK_CONFIG_MODE", default=False) is not "production": 
-        config.load_kube_config(app.config["KUBERNETES_CONFIG_PATH"])
-    else:
-        config.load_incluster_config()
+    if os.environ.get("KUBE_ENABLE", default=True):
+        if os.environ.get("FLASK_CONFIG_MODE", default=False) is "production": 
+            config.load_incluster_config()
+        else:
+            pass
+            config.load_kube_config(app.config["KUBERNETES_CONFIG_PATH"])
 
 
