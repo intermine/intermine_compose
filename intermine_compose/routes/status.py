4b9c81bc3b95858a907732890074a3b8f157f100
Ankur Kumar <ank@leoank.me>
2019-07-22 05:58:12 +0530

    Add method option to status route

--- intermine_compose/routes/status.py
+++ intermine_compose/routes/status.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 status_bp = Blueprint("status", __name__, url_prefix='/api/v1/status')
 
-@status_bp.route("/")
+@status_bp.route("/", methods=["GET"])
 def status():
     return "OK"
