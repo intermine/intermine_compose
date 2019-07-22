538695182d81ba4abee0897cc86cec83cffe3d4e
Ankur Kumar <ank@leoank.me>
2019-07-22 05:54:56 +0530

    Add secret key config for login manager

--- config/default.py
+++ config/default.py
@@ -6,3 +6,4 @@ CELERY_BROKER_URL = "redis://127.0.0.1:6379"
 KUBERNETES_CONFIG_PATH = f"{HOME}/.kube/config"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = "postgres://postgres:postgres@localhost:5432/composedb"
+SECRET_KEY = "ADD_A_RANDOM_KEY_HERE"
--- config/docker_local.py
+++ config/docker_local.py
@@ -1,3 +1,4 @@
 SQLALCHEMY_DATABASE_URI = "postgres://postgres:postgres@postgres:5432/composedb_dev"
 CELERY_BROKER_URL = "redis://redis:6379"
-FLASK_DEBUG = False
+FLASK_DEBUG = True
+DEV_DB = True
