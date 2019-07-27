bd4b567eb74a0be3559d286b0ea6249ff3b0b482
Ankur Kumar <ank@leoank.me>
2019-07-28 04:22:58 +0530

    Add configurator and data bp to init

--- intermine_compose/routes/__init__.py
+++ intermine_compose/routes/__init__.py
@@ -1,6 +1,10 @@
 from .users import user_bp
+from .configurator import configurator_bp
 from .status import status_bp
+from .data import data_bp
 
 def init_app(app):
     app.register_blueprint(user_bp)
     app.register_blueprint(status_bp)
+    app.register_blueprint(configurator_bp)
+    app.register_blueprint(data_bp)
