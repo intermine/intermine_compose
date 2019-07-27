7b73c1605c90010509292cf6332c752af5595c3e
Ankur Kumar <ank@leoank.me>
2019-07-28 04:20:36 +0530

    Update mine db model

--- intermine_compose/models/mine.py
+++ intermine_compose/models/mine.py
@@ -1,11 +1,19 @@
-from .base import db, TimestampMixin
+from .base import db, ma, TimestampMixin
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
+from marshmallow import fields
+from uuid import uuid4
 
 class Mine(TimestampMixin, db.Model):
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     url = Column(String(100), nullable=False)
-    config = Column(String(5000), nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), primary_key=True)
+    config = Column(String(), nullable=False)
+    templates = Column(String(), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"))
     user = relationship("User", back_populates="mines", lazy="joined", single_parent=True)
+    data_files = relationship("DataFile", back_populates="mine", lazy="joined")
+    builds = relationship("Build", back_populates="mine", lazy="joined")
+
+class MineStateSchema(ma.Schema):
+    state = fields.String(required=True)
