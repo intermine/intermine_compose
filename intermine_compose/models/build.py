b1482c775d60dc4fe01bb52e06900198fe4af245
Ankur Kumar <ank@leoank.me>
2019-07-29 10:47:49 +0530

    Update build db model and schema

--- intermine_compose/models/build.py
+++ intermine_compose/models/build.py
@@ -7,9 +7,17 @@ from uuid import uuid4
 
 class Build(TimestampMixin, db.Model):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    state = Column(String(100), nullable=False)
+    buildStatus = Column(String(100), nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"))
     user = relationship("User", back_populates="builds", lazy="joined", single_parent=True)
-    mine_id = Column(UUID(as_uuid=True), ForeignKey("mine.id"))
+    mineId = Column(UUID(as_uuid=True), ForeignKey("mine.id"))
     mine = relationship("Mine", back_populates="builds", lazy="joined", single_parent=True)
 
+
+class BuildStatusSchema(Schema):
+    mineId = fields.String(required=True)
+    buildStatus = fields.String(required=True)
+    errorDetails = fields.String(required=False)
+
+class BuildTriggerSchema(Schema):
+    mineId = fields.String(required=True)
