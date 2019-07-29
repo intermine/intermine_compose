41362da40a592eb31721382f9e051d5f3001477f
Ankur Kumar <ank@leoank.me>
2019-07-29 10:28:32 +0530

    Update mine db model and schema
    -  Add new fields
    - Add mineCheckName schema
    - Add mineSchema

--- intermine_compose/models/mine.py
+++ intermine_compose/models/mine.py
@@ -7,7 +7,10 @@ from uuid import uuid4
 
 class Mine(TimestampMixin, db.Model):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    url = Column(String(100), nullable=False)
+    mineName = Column(String(), unique=True, nullable=False)
+    minelocation = Column(String(100), unique=True, nullable=False)
+    mineStatus = Column(String(), nullable=False)
+    privacy = Column(String(), nullable=False)
     config = Column(String(), nullable=False)
     templates = Column(String(), nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"))
@@ -15,5 +18,15 @@ class Mine(TimestampMixin, db.Model):
     data_files = relationship("DataFile", back_populates="mine", lazy="joined")
     builds = relationship("Build", back_populates="mine", lazy="joined")
 
+class MineSchema(ma.Schema):
+    mineName = fields.String(required=True)
+    minelocation = fields.Url(required=True)
+    mineStatus = fields.String(required=True)
+    privacy = fields.String(required=True)
+
 class MineStateSchema(ma.Schema):
-    state = fields.String(required=True)
+    mineStatus = fields.String(required=True)
+
+class MineCheckNameSchema(ma.Schema):
+    mineName = fields.String(required=True)
+    isAvailable = fields.String(required=False)
