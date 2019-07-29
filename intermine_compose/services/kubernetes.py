9b4a889723de7574c9b0ea4ae647dcb15d4e1768
Ankur Kumar <ank@leoank.me>
2019-07-29 10:50:10 +0530

    Make kubernetes optional
    - makes it easier to test

--- intermine_compose/services/kubernetes.py
+++ intermine_compose/services/kubernetes.py
@@ -2,11 +2,9 @@ from kubernetes import client, config
 import os
 
 def init_kubernetes(app):
-    if os.environ.get("KUBE_ENABLE", default=True):
+    if os.environ.get("KUBE_ENABLE", default=False) is "True":
         if os.environ.get("FLASK_CONFIG_MODE", default=False) is "production": 
             config.load_incluster_config()
         else:
-            pass
             config.load_kube_config(app.config["KUBERNETES_CONFIG_PATH"])
-
 
