90ee6ef2ba3c20fd7e736479c7436a09a3158e0a
Ankur Kumar <ank@leoank.me>
2019-08-05 13:04:38 +0530

    Add error handling in configurator routes
    Fixes #12

--- intermine_compose/routes/configurator.py
+++ intermine_compose/routes/configurator.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, abort
 from requests import request as rq
 from flask_login import login_required, current_user
 import os
+from http import HTTPStatus
+
+# did relative imports here (Make sure to change this during refactoring )
+from ..utils import custom_logger
 
 configurator_bp = Blueprint("configurator", __name__, url_prefix='/api/v1/configurator')
 
@@ -12,7 +16,9 @@ configurator_bp = Blueprint("configurator", __name__, url_prefix='/api/v1/config
 def proxy(path):
     args = request.args.to_dict(flat=False)
     args["userId"] = current_user.get_id()
-    resp = rq(
+    
+    try:
+        resp = rq(
         method=request.method,
         # url=request.url.replace(request.host_url, 'new-domain.com'),
         url=f'{os.environ.get("CONFIGURATOR_URL")}{path}',
@@ -21,7 +27,11 @@ def proxy(path):
         data=request.get_data(),
         # cookies=request.cookies,
         allow_redirects=False
-    )
+        )
+    except:
+        custom_logger.handle.error("FAILED TO REACH CONFIGURATOR")
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR, str("FAILED TO REACH CONFIGURATOR"))
+    
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = [(name, value) for (name, value) in resp.raw.headers.items()
                if name.lower() not in excluded_headers] 
