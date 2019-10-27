804e29c6d3c2a1f22853cbf0dfa19653e376d1d7
Ankur Kumar <ank@leoank.me>
2019-10-28 00:12:01 +0530

    Fix typo in travis config and docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,9 @@
+echo "Attempting docker login"
 docker login -u "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD"
+echo "Staring docker build"
 docker build
       --tag "intermine/compose:latest"
       --tag "intermine/compose:$TRAVIS_COMMIT" .
+echo "Pushing built image to the registry "
 docker push "intermine/compose:latest"
-docker push "intermine/compose:$TRAVIS_COMMIT" .
+docker push "intermine/compose:$TRAVIS_COMMIT"
