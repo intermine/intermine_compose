12220f01e73ad10a14be674030683134f33e6678
Ankur Kumar <ank@leoank.me>
2019-10-28 00:24:28 +0530

    fix typo

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,8 @@
 echo "Attempting docker login"
 docker login -u "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD"
 echo "Staring docker build"
-docker build
-      --tag "intermine/compose:latest"
+docker build \
+      --tag "intermine/compose:latest" \
       --tag "intermine/compose:$TRAVIS_COMMIT" .
 echo "Pushing built image to the registry "
 docker push "intermine/compose:latest"
