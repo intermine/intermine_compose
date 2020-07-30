01b297ae4becef0699913b8ed1330564968999e8
Ankur Kumar <ank@leoank.me>
2020-07-30 20:27:37 +0530

    Refactor db init

--- src/intermine_compose/models/__init__.py
+++ src/intermine_compose/models/__init__.py
@@ -1,17 +1,6 @@
-from .base import db, ma
 from .users import User
 from .data import DataFile
+from . import meta
 from .mine import Mine
 from .templates import Template
 from .build import Build
-
-def init_app(app):
-    db.init_app(app)
-    # ma.init_app(app)
-
-
-def create_all_tables():
-    db.create_all()
-
-def drop_all_tables():
-    db.drop_all()
