4c29cf8012a0e99c9e2ec0b0255b62e2bbb3651d
Ankur Kumar <ank@leoank.me>
2020-08-07 22:02:54 +0530

    Refactor app for FastAPI

--- src/intermine_compose/app.py
+++ src/intermine_compose/app.py
@@ -1,48 +1,48 @@
 """Intermine Compose App."""
 
-from typing import Optional
+from typing import Any
 
+from fastapi import FastAPI, Request, Response
 from flask import Flask
 from logzero import logger
 
-from intermine_compose.config import Config
-from intermine_compose.extentions import bcrypt, cors, db, login_manager, migrate
+from intermine_compose.database import db
+from intermine_compose.extentions import bcrypt, cors, login_manager, migrate
 from intermine_compose.routes import (
     # build_bp,
     # configurator_bp,
     # data_bp,
-    status_bp,
+    status_route,
     # mine_bp,
-    user_bp,
+    # user_bp,
 )
 
 
-def create_app(config: Optional[Config] = None) -> Flask:
+def create_app() -> Flask:
     """App factory."""
-    app = Flask(__name__, template_folder="./templates")
+    app = FastAPI()  # Flask(__name__, template_folder="./templates")
     logger.info("App instance created")
 
-    # Loads default config defined in config dir
-    app.config.from_object("intermine_compose.config.default")
+    # Register app middlewares
 
-    # Loads specific config defined in config dir
-    if config:
-        app.config.from_object(f"intermine_compose.config.{config.value}")
-        logger.debug(f"Config loaded: {config.value}")
-    else:
-        logger.debug("Config loaded: default")
-    # Register app extentions
-    register_extensions(app)
-    logger.debug("Extensions loaded")
+    # db session handler
+    @app.middleware("http")
+    async def db_session_handler(request: Request, call_next: Any) -> Response:
+        db.connect()
+        response = await call_next(request)
+        db.close()
+        return response
 
-    # register blueprints
-    register_blueprints(app)
-    logger.debug("Blueprints registered")
+    logger.debug("Middlewares loaded")
+
+    # register routers
+    register_routers(app)
+    logger.debug("Routers registered")
 
     return app
 
 
-def register_extensions(app: Flask) -> None:
+def register_extensions(app: FastAPI) -> None:
     """Register Flask extensions."""
     bcrypt.init_app(app)
     cors.init_app(app, supports_credentials=True)
@@ -52,10 +52,10 @@ def register_extensions(app: Flask) -> None:
     return None
 
 
-def register_blueprints(app: Flask) -> None:
+def register_routers(app: FastAPI) -> None:
     """Register Flask blueprints."""
-    app.register_blueprint(user_bp)
-    app.register_blueprint(status_bp)
+    # app.register_blueprint(user_bp)
+    app.include_router(status_route, prefix="/v1/status", tags=["status"])
     # app.register_blueprint(configurator_bp)
     # app.register_blueprint(data_bp)
     # app.register_blueprint(mine_bp)
