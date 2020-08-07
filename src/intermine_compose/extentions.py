6a25fc3a55f6cbec5f72f59bd2c156a4dece441e
Ankur Kumar <ank@leoank.me>
2020-08-07 22:04:09 +0530

    Refactor config loading

--- src/intermine_compose/extentions.py
+++ src/intermine_compose/extentions.py
@@ -5,19 +5,27 @@ from flask_bcrypt import Bcrypt  # type: ignore
 from flask_cors import CORS
 from flask_login import LoginManager  # type: ignore
 from flask_migrate import Migrate  # type: ignore
-from flask_sqlalchemy import SQLAlchemy  # type: ignore
+from logzero import logger
 import marshmallow
 from sendgrid import SendGridAPIClient
+
+
+from intermine_compose.config import Config, get_config
+
+# initialize config
+env = Env()
+env.read_env()
+app_config = env.str("APP_CONFIG", "DEFAULT")
+logger.info(f"Config loaded: {app_config}")
+settings = get_config(Config[app_config])
 
 bcrypt = Bcrypt()
 cors = CORS()
 login_manager = LoginManager()
-db: SQLAlchemy = SQLAlchemy()
+
 migrate = Migrate()
 ma = marshmallow
 
-env = Env()
-env.read_env()
 
 sendGrid: SendGridAPIClient = SendGridAPIClient(
     api_key=env.str("SENDGRID_API_KEY", default="GIVE_ME_SENDGRID_KEY")
