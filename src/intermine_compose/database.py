55e3d47f777534569f41c1131c2aeb6240f19c64
Ankur Kumar <ank@leoank.me>
2020-09-01 09:52:55 +0530

    refactor db connection for pewee

--- src/intermine_compose/database.py
+++ src/intermine_compose/database.py
@@ -1,7 +1,13 @@
 """App database."""
 
+from contextvars import ContextVar
+from typing import Any
+
+from fastapi import Depends
 from logzero import logger
+import peewee
 from playhouse.postgres_ext import PostgresqlExtDatabase
+from pydantic.utils import GetterDict
 
 from intermine_compose.config import DefaultConfig
 from intermine_compose.extentions import settings
@@ -17,11 +23,62 @@ def create_db(config: DefaultConfig) -> PostgresqlExtDatabase:
         port=config.DB_PORT,
         autorollback=False,
         autocommit=False,
+        # check_same_thread=False,
     )
+
+
+# hacks to make peewee work with FastAPI
+db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
+db_state = ContextVar("db_state", default=db_state_default.copy())
+
+
+class PeeweeConnectionState(peewee._ConnectionState):
+    """Modified peewee connection state."""
+
+    def __init__(self: "PeeweeConnectionState", **kwargs) -> None:
+        """Initialize class."""
+        super().__setattr__("_state", db_state)
+        super().__init__(**kwargs)
+
+    def __setattr__(self: "PeeweeConnectionState", name: Any, value: Any) -> None:
+        """Add attr."""
+        self._state.get()[name] = value
+
+    def __getattr__(self: "PeeweeConnectionState", name: Any) -> Any:
+        """Get attr."""
+        return self._state.get()[name]
+
+
+class PeeweeGetterDict(GetterDict):
+    """Convert pewee model instances to pydantic."""
+
+    def get(self: "PeeweeGetterDict", key: Any, default: Any = None):
+        """Get dict."""
+        res = getattr(self._obj, key, default)
+        if isinstance(res, peewee.ModelSelect):
+            return list(res)
+        return res
 
 
 # create a db object shared by entire app
 db = create_db(settings)
+db._state = PeeweeConnectionState()
+
+
+async def reset_db_state() -> None:
+    """Reset DB state."""
+    db._state._state.set(db_state_default.copy())
+    db._state.reset()
+
+
+def get_db(db_state: Any = Depends(reset_db_state)) -> Any:  # noqa
+    """Provides db connection to route handlers."""
+    try:
+        db.connect()
+        yield
+    finally:
+        if not db.is_closed():
+            db.close()
 
 
 # hack for now to workaround circular import
@@ -84,4 +141,3 @@ def reset_db(db: PostgresqlExtDatabase) -> None:
     except BaseException as e:
         logger.error(f"Failed to reset tables\n\n Error Message: {e} \n\n")
     db.close()
-
