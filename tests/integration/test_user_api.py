6215b0c8a8d8f19c961c160fdfb3328dbe20ef37
Ankur Kumar <ank@leoank.me>
2020-09-04 00:13:41 +0530

    add user api tests

--- tests/integration/test_user_api.py
+++ tests/integration/test_user_api.py
@@ -1,112 +1,39 @@
 """Tests for User API."""
 
-# from flask_sqlalchemy import SQLAlchemy
+from fastapi.testclient import TestClient
 import pytest
+from requests.cookies import RequestsCookieJar
 
 from intermine_compose.models.actor import Actor
-from ..factories import ActorFactory, UserRegisterSchemaObjFactory
+from ..factories import UserRegisterSchemaObjFactory
 
 
-# @pytest.mark.usefixtures("db")
-# class TestUserApi:
-#     """User API tests."""
-
-#     def test_can_register(self: "TestUserApi", testapp: TestApp) -> None:
-#         """Register a new user."""
-#         old_count = len(Actor.query.all())
-
-#         # Post User details to user endpoint
-#         for _ in range(0, 10):
-#             params = RegisterSchemaObjFactory()
-#             res: TestResponse = testapp.post_json(
-#                 url="/api/v1/user/register", params=params, status="*",
-#             )
-
-#             assert res.status_code == 200
-#         # new users created
-#         assert len(Actor.query.all()) == old_count + 10
-
-#     def test_sees_error_message_if_user_already_registered(
-#         self: "TestUserApi", user: Actor, testapp: TestApp
-#     ) -> None:
-#         """Show error if user already registered."""
-#         user = ActorFactory(active=True)  # A registered user
-#         user.save()
-
-#         # Post User details to user endpoint
-#         res: TestResponse = testapp.post_json(
-#             url="/api/v1/user/register",
-#             params={
-#                 "name": "Bruce Wayne",
-#                 "email": user.email,
-#                 "organisation": "Wayne Enterprises",
-#                 "password": "iAMBetterThanStark",
-#             },
-#             status="*",
-#         )
-#         # sees error
-#         assert "email is already present" in str(res.json)
+@pytest.mark.usefixtures("db")
+def test_can_register(testclient: TestClient) -> None:
+    """Register a new user."""
+    old_count = len(Actor.select())
+    for _ in range(0, 10):
+        params = UserRegisterSchemaObjFactory()
+        res = testclient.post(url="/v1/user/", json=params)
+        assert res.status_code == 200
+    # new users created
+    assert len(Actor.select()) == old_count + 10
 
 
-# def test_register(client):
-#     resp = client.post(
-#         "/api/v1/user/register",
-#         json={
-#             "email": "john@doe.me",
-# 	        "password": "superpass",
-# 	        "firstName": "John",
-# 	        "lastName": "Doe",
-# 	        "organisation": "InterMine"
-#         }
-#     )
-#     assert resp.status_code == 200
-
-# def test_login(client):
-#     resp = client.post(
-#         "/api/v1/user/login",
-#         json={
-#             "email": "john@doe.me",
-#             "password": "superpass"
-#         }
-#     )
-#     assert resp.status_code == 200
-
-# def test_get_profile(client):
-#     resp = client.get(
-#         "api/v1/user/profile"
-#     )
-#     assert resp.status_code == 200
-#     assert resp.get_json() == {
-#         "email": "john@doe.me",
-# 	    "firstName": "John",
-# 	    "lastName": "Doe",
-# 	    "organisation": "InterMine"
-#     }
-
-# def test_update_profile(client):
-#     resp = client.post(
-#         "api/v1/user/profile",
-#         json={
-# 	        "firstName": "JohnNew",
-# 	        "lastName": "DoeNew",
-# 	        "organisation": "InterMineNew"
-#         }
-#     )
-#     assert resp.status_code == 200
-#     resp = client.get(
-#         "api/v1/user/profile"
-#     )
-#     assert resp.status_code == 200
-#     assert resp.get_json() == {
-#         "email": "john@doe.me",
-# 	    "firstName": "JohnNew",
-# 	    "lastName": "DoeNew",
-# 	    "organisation": "InterMineNew"
-#     }
+@pytest.mark.usefixtures("db")
+def test_sees_error_message_if_user_already_registered(
+    user: Actor, testclient: TestClient
+) -> None:
+    """Show error if user already registered."""
+    params = UserRegisterSchemaObjFactory.build(email=user.email)
+    res = testclient.post(url="/v1/user/", json=params)
+    # sees error
+    assert "email is already present" in str(res.json())
 
 
-# def test_logout(client):
-#     resp = client.get(
-#         "api/v1/user/logout"
-#     )
-#     assert resp.status_code == 200
+def test_can_get_profile(
+    user: Actor, testclient: TestClient, cookie_jar: RequestsCookieJar
+) -> None:
+    """Show error if user already registered."""
+    res = testclient.get(url="/v1/user/", cookies=cookie_jar)
+    assert res.status_code == 200
